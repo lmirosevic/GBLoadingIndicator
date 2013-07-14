82ea655718cb400170f36edddfffb41161ce45ad
Luka Mirosevic <luka@goonbee.com>
2013-07-14 14:13:28 +0200

    Initial Commit

---
+++ GBLoadingIndicator/GBLoadingIndicator.h
@@ -0,0 +1,13 @@
+//
+//  GBLoadingIndicator.h
+//  GBLoadingIndicator
+//
+//  Created by Luka Mirosevic on 14/07/2013.
+//  Copyright (c) 2013 Goonbee. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface GBLoadingIndicator : NSObject
+
+@end
---
+++ GBLoadingIndicator/GBLoadingIndicator.m
@@ -0,0 +1,13 @@
+//
+//  GBLoadingIndicator.m
+//  GBLoadingIndicator
+//
+//  Created by Luka Mirosevic on 14/07/2013.
+//  Copyright (c) 2013 Goonbee. All rights reserved.
+//
+
+#import "GBLoadingIndicator.h"
+
+@implementation GBLoadingIndicator
+
+@end
